5bef9ee0055656b3c067ef5b555475b15501f4e3
Lucien Peslier <lpeslier@meilleursagents.com>
2021-06-16 11:09:26 +0200

    refactor(name): Rename nut to npm-ut

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import generateMd from './generateMd';
 import readConfig from './readConfig';
 import updateTool from './updateTool';
 
-readConfig('./.nutrc.js').then((options) => {
+readConfig('./.npm-utrc.js').then((options) => {
   updateTool(options).then(async ({ groupList }) => {
     generateMd(groupList);
   });
