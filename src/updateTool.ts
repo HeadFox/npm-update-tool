9070064d4c7e4afbff04ab79f603d1d189a1ae8a
Lucien Peslier <lpeslier@meilleursagents.com>
2021-06-16 18:45:48 +0200

    fix(comment): Remove comment of installation

--- src/updateTool.ts
+++ src/updateTool.ts
@@ -37,7 +37,7 @@ const updateTool = async (
   try {
     signale.info('Install & quality tests before start');
     interactive.await('Installing');
-    // await exec('npm install');
+    await exec('npm install');
     await qualityTest(
       'pre-quality',
       { test: true, lint: true, build: true },
