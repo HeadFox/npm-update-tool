e62e5fcfd9b7c16e788a82279d602472cfbad8c0
Lucien Peslier <lpeslier@meilleursagents.com>
2021-06-21 14:24:30 +0200

    chore(sass): Add sass to buildFilter

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -7,5 +7,5 @@ const categories = [
 ];
 
 const lintFilter = ['prettier', 'eslint', 'stylelint'];
-const buildFilter = ['typescript', 'react-scripts', 'next', 'rollup'];
+const buildFilter = ['typescript', 'react-scripts', 'next', 'rollup', 'sass'];
 export { categories, lintFilter, buildFilter };
